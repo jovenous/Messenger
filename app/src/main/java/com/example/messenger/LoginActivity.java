f85adebf5cdd2ba44fece509997988417ed5bbf2
Jovenous <serj2.cn@gmail.com>
2024-01-21 20:02:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/messenger/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewForgotPassword;
+    private TextView textViewRegister;
+    private Button buttonLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initViews();
+    }
+
+    private void initViews() {
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
+        textViewRegister = findViewById(R.id.textViewRegister);
+        buttonLogin = findViewById(R.id.buttonLogin);
+    }
+}
