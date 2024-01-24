b032514d14632f0ea52582882ea17e47db95c029
Jovenous <serj2.cn@gmail.com>
2024-01-24 21:28:54 +0200

    commit 9 / ChatActivity layout

---
+++ app/src/main/java/com/example/messenger/ChatActivity.java
@@ -0,0 +1,41 @@
+package com.example.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private TextView textViewTitle;
+    private View onlineStatus;
+    private RecyclerView recyclerViewChat;
+    private EditText editTextMessage;
+    private ImageView imageViewSendMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        initViews();
+    }
+
+    private void initViews() {
+        textViewTitle = findViewById(R.id.textViewTitle);
+        onlineStatus = findViewById(R.id.onlineStatus);
+        recyclerViewChat = findViewById(R.id.recyclerViewChat);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, ChatActivity.class);
+    }
+}
